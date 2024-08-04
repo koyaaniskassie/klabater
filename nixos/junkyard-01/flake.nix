8bc53de83fa8f0b21862162481cbad741dd804db
koyaaniskassie <kassien4092@gmail.com>
2024-08-04 17:56:19 +0200

    upgraded config

--- nixos/junkyard-01/flake.nix
+++ nixos/junkyard-01/flake.nix
@@ -16,14 +16,15 @@
           };
       in
       {
-        nixosConfigurations.k8s-master = nixpkgs.lib.nixosSystem {
+        nixosConfigurations.junkyard-01 = nixpkgs.lib.nixosSystem {
             inherit system;
             modules = [
+              /etc/nixos/configuration.nix
               ({ config, pkgs, ... }: {
                   boot.isContainer = false;
 
                   networking = {
-                      hostName = "k8s-master";
+                      hostName = "junkyard-01";
                       firewall.allowedTCPPorts = [
                         6443 
                         2379 
@@ -36,8 +37,8 @@
 
                   services.kubernetes = {
                       roles = ["master"];
-                      masterAddress = "k8s-master";
-                      apiserverAddress = "https://k8s-master:6433";
+                      masterAddress = "junkyard-01";
+                      apiserverAddress = "https://junkyard-01:6433";
                       easyCerts = true;
                       apiServer = {
                           extraOpts = "--allow-privileged";
@@ -47,11 +48,30 @@
                   environment.systemPackages = with pkgs; [
                     kubectl
                     kubernetes
-                  ]
+                    vim
+                    git
+                    wget
+                    curl
+                  ];
+
+                  users.users.kassie = {
+                      isNormalUser = true;
+                      home = "/home/kassie";
+                      extraGroups = [ "wheel" "networkmanager" "docker" ];
+                      hashedPassword = "$6$UJF4nxZgr0sWa87q$OL.oo1txNlzy9SrxWs0N8aN3Ox9PttBXfpbBcOviS67qLN8EWWieqWBpm4OlK1ZiA3lWisI7gwyhvtKC9JZem/"
+                      openssh.authorizedKeys.keys = [
+                        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZdUM+ZCQ0Odgh3KDHw1Fk/iC9L/jE4muEjbqnU/465vVfOngSc/lJGydPg/a8NfBxt8SMSqJXPHgi7UFI8rv9utbst0xawW76KHtlsfSzNQrantRmlyUxCY0LAjSE1pdsk1p2TrKT3jttLUxPGjXCxRkV4P0FHmjASsbVgEhYagyKRLXTb722+BRkZbQqXQPu5xBLVBMOIlQiLnbC0riLblBlNX8Bv44nWiUgmscuz4vlQNudKgcJn0JkGE5hnI0tMoaHNGsz9tjHxKUbMavOfsQ9mzXqH0WPsdvXbaEC6yWrR+e6ODl1tC0byn7OuP89QyFfimuBO2xdV1KVsn1ZPdWCJkgLjYPy5OkjGZttU/wzcQo8SFYwaxsr4QK+nx3ploLSZyjjP8YWqUkl1r3vFB4x9M7B7EkGzITXfAhNwHDjv4CiKJk24mULOLe4zg07Ek4ax0N8eHjeg4RvjKvitCFnDIacoyNpNpKqmwYVKLbNyHtD0WVHjshhNOL5DeAf/GnSEVhkA939KVNIuDrgnQyHFOu2gM+wEX7iZUtkooOSmLxan7WAJoLhX0Es4Izl0zabQA7IerspMKnvx5R8Mms2tyDrws+rcWJRKXtd4lz4Iz9Bn1m7TFQ1KFZMdYcGSEpoJZHxo3jOAJiFmpSzxatBt12XfEBCEQSodDMx5Q== kassie@fedora-2.home"
+                      ];
+                    };
+
                 })
-            ]
-          }
+
+            ];
+
+          };
+
         }
-    )
+
+    );
 
 }
