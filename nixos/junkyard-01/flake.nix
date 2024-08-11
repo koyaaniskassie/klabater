8e386c9248b04372a48789d35729abdcd8ce420a
koyaaniskassie <kassien4092@gmail.com>
2024-08-11 17:07:27 +0200

    ADD kubernetes group to user

--- nixos/junkyard-01/flake.nix
+++ nixos/junkyard-01/flake.nix
@@ -46,7 +46,7 @@
               isNormalUser = true;
               description = "Kubernetes administrator";
               home = "/home/kassie";
-              extraGroups = [ "wheel" "networkmanager" "docker" ];
+              extraGroups = [ "wheel" "networkmanager" "docker" "kubernetes" ];
               hashedPassword = "$6$UJF4nxZgr0sWa87q$OL.oo1txNlzy9SrxWs0N8aN3Ox9PttBXfpbBcOviS67qLN8EWWieqWBpm4OlK1ZiA3lWisI7gwyhvtKC9JZem/";
               openssh.authorizedKeys.keys = [
                 "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZdUM+ZCQ0Odgh3KDHw1Fk/iC9L/jE4muEjbqnU/465vVfOngSc/lJGydPg/a8NfBxt8SMSqJXPHgi7UFI8rv9utbst0xawW76KHtlsfSzNQrantRmlyUxCY0LAjSE1pdsk1p2TrKT3jttLUxPGjXCxRkV4P0FHmjASsbVgEhYagyKRLXTb722+BRkZbQqXQPu5xBLVBMOIlQiLnbC0riLblBlNX8Bv44nWiUgmscuz4vlQNudKgcJn0JkGE5hnI0tMoaHNGsz9tjHxKUbMavOfsQ9mzXqH0WPsdvXbaEC6yWrR+e6ODl1tC0byn7OuP89QyFfimuBO2xdV1KVsn1ZPdWCJkgLjYPy5OkjGZttU/wzcQo8SFYwaxsr4QK+nx3ploLSZyjjP8YWqUkl1r3vFB4x9M7B7EkGzITXfAhNwHDjv4CiKJk24mULOLe4zg07Ek4ax0N8eHjeg4RvjKvitCFnDIacoyNpNpKqmwYVKLbNyHtD0WVHjshhNOL5DeAf/GnSEVhkA939KVNIuDrgnQyHFOu2gM+wEX7iZUtkooOSmLxan7WAJoLhX0Es4Izl0zabQA7IerspMKnvx5R8Mms2tyDrws+rcWJRKXtd4lz4Iz9Bn1m7TFQ1KFZMdYcGSEpoJZHxo3jOAJiFmpSzxatBt12XfEBCEQSodDMx5Q== kassie@fedora-2.home"
