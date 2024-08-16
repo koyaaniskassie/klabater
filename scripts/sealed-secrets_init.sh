2a2d08334436048443cc69399707a0f3e6a80ef6
koyaaniskassie <kassien4092@gmail.com>
2024-08-16 15:30:41 +0200

    ADD dowload sealed-secrets certificate

--- scripts/sealed-secrets_init.sh
+++ scripts/sealed-secrets_init.sh
@@ -5,3 +5,8 @@ KUBESEAL_VERSION='0.27.1'
 curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
 tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+echo "installed sealed-secrets on cluster and kubeseal on your machine"
+
+echo "downloading certificate"
+kubeseal --fetch-cert >~/.kube/kubeseal_local_cert.pem
+echo "certificate installed in ~/.kube/kubeseal_local_cert.pem"
