37bd0c36a9a99e818e90ca59b8b79405e0a90676
koyaaniskassie <kassien4092@gmail.com>
2024-08-16 13:53:11 +0200

    CHANGE helm install command to support default setup

--- scripts/sealed-secrets.sh
+++ scripts/sealed-secrets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm install sealed-secrets-cluster sealed-secrets/sealed-secrets
+helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
