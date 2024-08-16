e7b8efb5464feebf398bbc12c454b1915dd9b564
koyaaniskassie <kassien4092@gmail.com>
2024-08-16 14:05:08 +0200

    REMOVE unnecessary shebang

--- scripts/sealed-secrets.sh
+++ scripts/sealed-secrets.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
