e1000f666247f0e65ef518617783b64f4649892b
koyaaniskassie <kassien4092@gmail.com>
2024-08-16 15:08:23 +0200

    ADD kubeseal installation

--- scripts/sealed-secrets.sh
+++ scripts/sealed-secrets.sh
@@ -1,2 +1,7 @@
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
+
+KUBESEAL_VERSION='0.27.1'
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
