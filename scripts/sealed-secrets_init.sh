dfb361566e7f8f4522af4af40f65294bcb7513f3
koyaaniskassie <kassien4092@gmail.com>
2024-08-18 02:38:06 +0200

    ADD helm chart for sealed secrets

--- scripts/sealed-secrets_init.sh
+++ scripts/sealed-secrets_init.sh
@@ -1,5 +1,6 @@
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
-helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
+cd ../kubernetes/sealed-secrets/
+helm dependency update
+helm install sealed-secrets . --namespace kube-system -f values.yaml
 
 KUBESEAL_VERSION='0.27.1'
 curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION:?}/kubeseal-${KUBESEAL_VERSION:?}-linux-amd64.tar.gz"
